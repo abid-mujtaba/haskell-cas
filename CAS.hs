04ab6c6a739a29deccf3c6b2448703566e08d013
Abid H Mujtaba <abid.naqvi83@gmail.com>
2015-01-11 22:07:31 +0500

    Renames the module file to have its name that matches that of the module (CAS).

---
+++ CAS.hs
@@ -0,0 +1,21 @@
+-- Author: Abid H. Mujtaba
+-- Date: 2015-01-11
+
+-- This module implements a Computer Algebra System for Haskell.
+--
+-- This work is heavily inspired by (basically a minor modification of): https://github.com/hepek/Ramblings/blob/master/symb.lhs
+
+
+-- We start by declaring this .hs file to be module. The module must have the same name as the file (CAS.hs).hs
+-- In the module identifier after the module's name and delimited in parentheses we declare the classes, types and functions we want to export from the module.
+-- The parentheses are followed by the keyword where and then the rest of the file is dedicated to defining the various objects that are being exported.
+
+module CAS
+    (
+--      Expr(...)
+--      , x, y, z, a, b, c
+--      , simplify
+--      , diff
+--      , eval
+    )
+    where
