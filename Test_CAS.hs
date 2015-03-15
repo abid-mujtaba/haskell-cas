fd80ff56fa024ec85a891329f5893e2dfbd1e2f8
Abid H Mujtaba <abid.naqvi83@gmail.com>
2015-03-15 12:33:46 +0500

    Use Vars in Test to load variables

--- Test_CAS.hs
+++ Test_CAS.hs
@@ -49,35 +49,12 @@ import Test.HUnit
 import Test.QuickCheck
 
 import CAS
+import Vars
 
 main = do
           runTestTT tests          -- In the main function we simply run the tests. So running the executable (TestCAS) will now cause the tests to be executed
           quickTests
 
-
--- Define the first 10 positive and 9 negative integers for testing.
-z0, z1, z2, z3, z4, z5, z6, z7, z8, z9 :: (Integral a) => Expr a
-z0 = const' 0
-z1 = const' 1
-z2 = const' 2
-z3 = const' 3
-z4 = const' 4
-z5 = const' 5
-z6 = const' 6
-z7 = const' 7
-z8 = const' 8
-z9 = const' 9
-
-zm1, zm2, zm3, zm4, zm5, zm6, zm7, zm8, zm9 :: (Integral a) => Expr a
-zm1 = const' (-1)
-zm2 = const' (-2)
-zm3 = const' (-3)
-zm4 = const' (-4)
-zm5 = const' (-5)
-zm6 = const' (-6)
-zm7 = const' (-7)
-zm8 = const' (-8)
-zm9 = const' (-9)
 
 
 -- We add a label to the TestCase by using the TestLabel constructor.
