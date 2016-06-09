f56a21eb157e8f1f18eb52ccbaa623cdeafe9c15
Abid H Mujtaba <abid.naqvi83@gmail.com>
2016-06-09 21:48:16 +0500

    Make Expr an instance of Num and implement (+) naively

---
+++ src/Expr/Num.hs
@@ -0,0 +1,17 @@
+-- | The Expr.Num module makes the Expr type an instance of the Num class allowing expressions to engage in arithmetic operations which form the core of the functionality of a CAS
+module Expr.Num
+    (
+        (+)         -- Explicitly export the + function. Functions not explicitly exported will raise an exception if they are called.
+    )
+    where
+
+import Expr (Expr(Add))
+
+instance Num Expr where
+    -- ToDo: Implement using separate module and deal with commutation
+    x + y           = Add x y
+    -- x * y           = error "Not implemented yet"
+    -- x - y           = error "Not implemented yet"
+    -- abs x           = error "Not implemented yet"
+    -- signum x        = error "Not implemented yet"
+    -- fromInteger c   = error "Not implemented yet"
