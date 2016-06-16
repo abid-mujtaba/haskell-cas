e4588b5209d89a57991b91939f16a22eeb725619
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2016-06-16 16:03:54 +0500

    Create separate functions for showing coeff and power of Atom

--- src/Expr/Show.hs
+++ src/Expr/Show.hs
@@ -13,10 +13,21 @@ import Expr (Expr (Symbol, Atom, Add))           -- We need access to ALL constr
 instance Show Expr where
     show (Symbol s) = s                                                 -- Inside every Symbol is a String. We simply tell 'show' that for a Symbol object simply show the String inside
     show (Atom c _ 0)     = show c                            -- A constant is detected when we have an atom with power 0. This pattern detects a constant and renders it by showing simply the coefficient
-    show (Atom 1 e 1) = show e                                          -- If the atom has coeff 1 and power 1 then simply show the core expr
-    show (Atom 1 e p) = show e ++ "^" ++ show p                         -- If the atom has coeff 1 then do not display the coeff
-    show (Atom c e p) = show c ++ " " ++ show e ++ "^" ++ show p        -- We use String concatenation to define how an atom is to be represented as a String
+    show (Atom c e p) = showCoeff c ++ show e ++ showPower p            -- We use String concatenation to define how an atom is to be represented as a String. We define separate functions for dealing with the constant and power parts of an atom
     show (Add x y)    = "(" ++ showSum x ++ " + " ++ showSum y ++ ")"   -- Added expressions are always surrounded by parentheses. Recursively added expressions share a single set of parentheses
+
+
+-- Define how to show the coefficient of an Atom is to be rendered/shown
+showCoeff :: Integer -> String
+showCoeff 1     = ""                    -- A coeff of 1 is not shown
+showCoeff (-1)  = "-"                   -- A coeff of -1 is simply shown with a minus sign
+showCoeff c     = show c ++ " "         -- The coeff is separated from the next part by a single space
+
+
+-- Define how the power of an Atomj is to be rendered
+showPower :: Integer -> String
+showPower 1     = ""                    -- A power of 1 is not shown
+showPower p     = "^" ++ show p
 
 
 -- Function for dealing with recursively added expressions
