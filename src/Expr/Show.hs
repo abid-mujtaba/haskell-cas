c0173fec4eae5e51d2ca0051b4af797b6b367d9f
Abid H Mujtaba <abid.naqvi83@gmail.com>
2016-07-10 18:20:25 +0500

    Add ability to show expressions containing Inv

--- src/Expr/Show.hs
+++ src/Expr/Show.hs
@@ -14,15 +14,16 @@ import qualified Expr.Add (negate)
 instance Show Expr where
     show (Symbol s) = s                                                 -- Inside every Symbol is a String. We simply tell 'show' that for a Symbol object simply show the String inside
     show (Atom c _ 0)     = show c                            -- A constant is detected when we have an atom with power 0. This pattern detects a constant and renders it by showing simply the coefficient
+    show (Atom c e@(Inv _) 1) = show c ++ show e              -- An Inv encapsulated inside an isolated Atom
     show (Atom 1 e 1)     = show e
     show (Atom c e p) = showCoeff c ++ show e ++ showPower p            -- We use String concatenation to define how an atom is to be represented as a String. We define separate functions for dealing with the constant and power parts of an atom
     -- Added expressions are always surrounded by parentheses. Recursively added expressions share a single set of parentheses
     show (Add x y@(Atom c _ _))
-        | c >= 0    = "(" ++ showSum x ++ " + " ++ showSum y ++ ")"
-        | otherwise = "(" ++ showSum x ++ " - " ++ showSum (Expr.Add.negate y) ++ ")"       -- If the second argument has negative coeff then we show a minus sign and use negate to flip the sign of the expression before showing it
-    show (Mul x (Atom 1 (Inv y) 1)) = show x ++ " / (" ++ show y ++ ")"
-    show (Mul x y) = show x ++ " " ++ show y
-    show (Inv y)        = "1 / (" ++ show y ++ ")"
+        | c >= 0    = "(" ++ showInSum x ++ " + " ++ showInSum y ++ ")"
+        | otherwise = "(" ++ showInSum x ++ " - " ++ showInSum (Expr.Add.negate y) ++ ")"       -- If the second argument has negative coeff then we show a minus sign and use negate to flip the sign of the expression before showing it
+    -- show (Mul x (Atom 1 (Inv y) 1)) = show x ++ " / (" ++ show y ++ ")"
+    show (Mul x y) = show x ++ showInMul y
+    show (Inv y)        = " / " ++ showInInv y          -- Isolated Inv
 
 -- Define how to show the coefficient of an Atom is to be rendered/shown
 showCoeff :: Integer -> String
@@ -38,13 +39,25 @@ showPower p     = "^" ++ show p
 
 
 -- Function for dealing with recursively added expressions
-showSum :: Expr -> String
+showInSum :: Expr -> String
 -- If an expression consists of an addition with NO coefficient and no power then it is included in the original set of parentheses defined by 'show (Add x y)'
 -- If the coeff of the second term is negative then a minus sign is shown as the connector
-showSum (Atom 1 (Add x y@(Atom c _ _)) 1)
-    | c < 0    = showSum x ++ " - " ++ showSum (Expr.Add.negate y)     -- Since we are showing a "-" sign we negate the second argument before we show it
-    | otherwise = showSum x ++ " + " ++ showSum y
-showSum e                     = show e                                  -- Every other kind of expression is then relegated back to the original show for rendering
+showInSum (Atom 1 (Add x y@(Atom c _ _)) 1)
+    | c < 0    = showInSum x ++ " - " ++ showInSum (Expr.Add.negate y)     -- Since we are showing a "-" sign we negate the second argument before we show it
+    | otherwise = showInSum x ++ " + " ++ showInSum y
+showInSum e                     = show e                                  -- Every other kind of expression is then relegated back to the original show for rendering
+
+
+-- Function for dealing with expressions inside an Inv
+showInInv :: Expr -> String
+showInInv e@(Atom _ (Mul _ _) _)       = "(" ++ show e ++ ")"           -- Products inside Inv should be surrounded by parentheses
+showInInv e                            = show e
+
+
+-- Function for dealing with expressions inside a Mul
+showInMul :: Expr -> String
+showInMul (Atom 1 ie@(Inv _) 1)   = show ie                             -- An inverse inside a Mul is shown with no space and rendering passed on to 'show' which deals with Inv properly
+showInMul b                       = " " ++ show b
 
 
 -- Utility function for showing the actual constructor and recursive nature of an expression.
@@ -53,3 +66,4 @@ showActual (Symbol s) = s
 showActual (Add a b) = "(Add " ++ showActual a ++ " " ++ showActual b ++ ")"
 showActual (Mul a b) = "(Mul " ++ showActual a ++ " " ++ showActual b ++ ")"
 showActual (Atom c e p) = "(Atom " ++ show c ++ " " ++ showActual e ++ " " ++ show p ++ ")"
+showActual (Inv e) = "(Inv " ++ show e ++ ")"
